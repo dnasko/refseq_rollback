8b478acbf76000d94632658a2fef8e318197db10
Dan Nasko <dnasko@fungcat00.umiacs.umd.edu>
2018-07-12 11:20:24 -0400

    More documentation added.

--- scripts/create_otu_table.pl
+++ scripts/create_otu_table.pl
@@ -2,6 +2,10 @@
 use strict;
 use warnings;
 
+## Creat a QIIME-Style OTU table from a set of RefSeq Catalog files
+## Pass in as many as you want. Each Catalog file is treated like a
+## sample in the OTU table.
+
 if (scalar(@ARGV) < 1) {
     die "\n Usaage: ./create_otu_table.pl /Path/to/catalog_files/*.gz > output_otu_table.txt\n\n";
 }
@@ -9,21 +13,23 @@ if (scalar(@ARGV) < 1) {
 my @Versions;
 my %Hash;
 
-foreach my $infile (@ARGV) {
-    my $version = $infile;
+foreach my $infile (@ARGV) { ## for each file passed to the script...
+    my $version = $infile;   ## get the version number
     $version =~ s/.*RefSeq-release//;
     $version =~ s/\.catalog\.gz//;
-    push(@Versions, $version);
+    push(@Versions, $version); ## add that version to the array
     print STDERR " $version started ... ";
     open(IN,"gunzip -c $infile|") || die "\n Cannot open the file: $infile\n";
     while(<IN>) {
 	chomp;
 	my @a = split(/\t/, $_);
-	$Hash{$a[0]}{$version}++;
+	$Hash{$a[0]}{$version}++; ## The first field is the NCBI taxonomy ID, read that into the dictionary
     }
     close(IN);
     print STDERR " [DONE]\n";
 }
+
+## Begin printing out the results...
 
 my $otu=1;
 print STDOUT "Org" . "\t" . "OTU" . "\t" . join("\t", @Versions) . "\n";
--- scripts/download_refseq_catalog.sh
+++ scripts/download_refseq_catalog.sh
@@ -2,7 +2,7 @@
 set -e
 
 ## Usage: ./download_refseq_catalog.sh 1
-## Where '1' can be any number corresponding to a version of RefSeq (1..88).
+## Where '1' can be any number corresponding to a version of RefSeq (from 1 to 88).
 
 VERSION=$1
 
