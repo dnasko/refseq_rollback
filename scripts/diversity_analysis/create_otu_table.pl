2869f6a3f55779665207dd90fb137f344ddc07c2
Dan Nasko <dan.nasko@gmail.com>
2018-07-23 13:46:16 -0400

    More comments and I created a directory of all the diversity-calculating scripts.

--- scripts/diversity_analysis/create_otu_table.pl
+++ scripts/diversity_analysis/create_otu_table.pl
@@ -3,8 +3,13 @@ use strict;
 use warnings;
 
 ## Creat a QIIME-Style OTU table from a set of RefSeq Catalog files
-## Pass in as many as you want. Each Catalog file is treated like a
+## that have had the taxonomic info dumped already from dump_taxonomy.pl.
+## Pass in as many as you want. Each dumped catalog file is treated like a
 ## sample in the OTU table.
+
+## Depending on the dump_taxonomy.pl script you used (e.g. dump_taxonomy_species.pl,
+## dump_taxonomy_genus.pl) will dictate at what level you're measuring
+## diversity.
 
 if (scalar(@ARGV) < 1) {
     die "\n Usaage: ./create_otu_table.pl /Path/to/catalog_files/*.gz > output_otu_table.txt\n\n";
@@ -15,22 +20,21 @@ my %Hash;
 
 foreach my $infile (@ARGV) { ## for each file passed to the script...
     my $version = $infile;   ## get the version number
-    $version =~ s/.*RefSeq-release//;
-    $version =~ s/\.catalog\.gz//;
+    $version =~ s/.*\///;
+    $version =~ s/\.tax//;
     push(@Versions, $version); ## add that version to the array
-    print STDERR " $version started ... ";
-    open(IN,"gunzip -c $infile|") || die "\n Cannot open the file: $infile\n";
+    print STDERR " $version started ... "; ## report what version you're working on
+
+    open(IN,"<$infile") || die "\n Cannot open the file: $infile\n";
     while(<IN>) {
 	chomp;
-	my @a = split(/\t/, $_);
-	$Hash{$a[0]}{$version}++; ## The first field is the NCBI taxonomy ID, read that into the dictionary
+	$Hash{$_}{$version}++; ## Each line is a species parsed by the dump_taxonomy.pl script
     }
     close(IN);
     print STDERR " [DONE]\n";
 }
 
 ## Begin printing out the results...
-
 my $otu=1;
 print STDOUT "Org" . "\t" . "OTU" . "\t" . join("\t", @Versions) . "\n";
 foreach my $i (sort keys %Hash) {
